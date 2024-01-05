d04b75949d5587bd18fccd84ce082d0bb4e4b60e
Renzxs <florencebatol85@gmail.com>
2024-01-05 17:21:44 +0800

    Added assets and node modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
 import './index.css';
 import NavBar from './components/NavBar';
+import StepNo1 from './assets/stepno1.svg';
+import StepNo2 from './assets/stepno2.svg';
+import StepNo3 from './assets/stepno3.svg';
+
 
 function RewardMainPage() {
   return (
@@ -16,14 +21,46 @@ function RewardMainPage() {
 }
 
 function GettingStartedPage() {
+
+  const [step, setStep] = useState([
+    {
+      image: StepNo1,
+      title: "Create an account",
+      description: "To get started, join now. join in the app to get access to the full range of Starbucks® Rewards benefits."
+    },
+    {
+      image: StepNo2,
+      title: "Create an account",
+      description: "To get started, join now. join in the app to get access to the full range of Starbucks® Rewards benefits."
+    },
+    {
+      image: StepNo3,
+      title: "Create an account",
+      description: "To get started, join now. join in the app to get access to the full range of Starbucks® Rewards benefits."
+    },
+
+  ]);
+
+
   return (
     <div className='p-10'>
-      <div className='text-center'>
+      <div className='text-center mb-10'>
         <h1 className='text-[28px] font-bold mb-2'>Getting started is easy</h1>
         <p className='text-[15px] font-medium'>Collect Stars and get rewarded in a few steps</p>
       </div>
-      <div>
 
+      <div className='flex justify-around items-center gap-10 px-20 max-lg:flex-col max-lg:px-0'>
+        {step.map((step) => {
+          return (
+            <div className='flex flex-col justify-center items-center text-center mt- gap-2 max-lg:flex-row max-lg:text-start'>
+              <img className='w-[51px]' src={step.image} alt={StepNo1} />
+              <div className='max-lg:ml-8'>
+                <h1 className='font-bold text-[18px] mb-4'>{step.title}</h1>
+                <p>{step.description}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   );
